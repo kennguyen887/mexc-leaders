da518caec8e04da8ea29d3f81cabcf799dbcefd5
ken nguyen <ngu.nt@carehealth.io>
2025-09-10 09:31:22 +0700

    feat: add proxy configuration to Vite server for API requests

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://7382bdf9.whale-futures.pages.dev',
+        changeOrigin: true,
+        secure: true,
+        // giữ nguyên đường dẫn /api/**
+        // (nếu backend không có prefix /api thì mới cần rewrite)
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
